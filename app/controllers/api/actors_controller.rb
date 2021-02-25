77bcc2542c76aeb104f2921f4ae0e6cdf8edd591
Eric <lee.erictae@gmail.com>
2021-02-25 01:10:00 -0600

    add one more actor, add actor view

--- app/controllers/api/actors_controller.rb
+++ app/controllers/api/actors_controller.rb
@@ -1,2 +1,7 @@
 class Api::ActorsController < ApplicationController
+  def actors
+    @actor = Actor.find_by(id: 1)
+    render "actors.json.jb"
+
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,5 +9,7 @@
 actor = Actor.create({first_name: "Benedict", last_name: "Cumberbatch", known_for: "Sherlock"})
 
 
+actor = Actor.create({first_name: "Chris", last_name: "Hemsworth", known_for: "Thor"})
+
 
 
