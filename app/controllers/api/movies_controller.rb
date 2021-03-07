90e7006e2627ab8186839d5c37f7490977705f9f
Eric <lee.erictae@gmail.com>
2021-03-06 20:34:06 -0600

    add create, show, update, destory action controller

--- app/controllers/api/movies_controller.rb
+++ app/controllers/api/movies_controller.rb
@@ -1,10 +1,37 @@
 class Api::MoviesController < ApplicationController
   def index
     @movies = Movie.all
-    render "movies.json.jb"
+    render "index.json.jb"
   end
   
   def create
-
+    @movie = Movie.new(
+      title: params[:title],
+      year: params[:year],
+      plot: params[:plot],
+    )
+    @movie.save
+    render "show.json.jb"
   end
+
+  def show
+    @movie = Movie.find_by(id: params[:id])
+    render "show.json.jb"
+  end
+
+  def update
+    @movie = Movie.find_by(id: params[:id])
+    @movie.title = params[:title]
+    @movie.year = params[:year]
+    @movie.plot = params[:plot]
+    @movie.save
+    render "show.json.jb"
+  end
+
+  def destroy
+    @movie = Movie.find_by(id: params[:id])
+    @movie.destroy
+    render json: {message: "movie has been deleted"}
+  end
+  
 end
